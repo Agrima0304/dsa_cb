8042b1a9bdd776990966df231dfabde94ff1672c
Agrima Bansal <86730647+Agrima0304@users.noreply.github.com>
2023-08-18 18:49:56 +0530

    Update Nqueen_problem.cpp

--- Nqueen_problem.cpp
+++ Nqueen_problem.cpp
@@ -2,74 +2,61 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool IsSafe(int mat[][1000],int i,int j,int n)
-{
-	//for row n column
-    for(int k=0;k<n;k++)
-	{
-		if(mat[i][k]==1||mat[k][j]==1)
-		{
-			return false;
-		}
-	}
-	while(i>=0&&j<n)
-	{
-		if(mat[i][j]==1)
-		{
-			return false;
-		}
-		i--,j++;
-	}
-		while(i>=0&&j>=0)
-	{
-		if(mat[i][j]==1)
-		{
-			return false;
-		}
-		i--,j--;
-	}
+bool IsSafe(int mat[][1000], int i, int j, int n) {
+    for (int k = 0; k < n; k++) {
+        if (mat[i][k] == 1 || mat[k][j] == 1) {
+            return false;
+        }
+    }
+    
+    for (int k = i, l = j; k >= 0 && l >= 0; k--, l--) {
+        if (mat[k][l] == 1) {
+            return false;
+        }
+    }
+    
+    for (int k = i, l = j; k >= 0 && l < n; k--, l++) {
+        if (mat[k][l] == 1) {
+            return false;
+        }
+    }
+    
     return true;
 }
-bool print(int mat[][1000],int i,int n,int &c)
-{
-	//Base Case
-	if(i==n)
-	{
-			c++;
-		for(int i=0;i<n;i++)
-		{
-			for(int j=0;j<n;j++)
-			{
-				if(mat[i][j]==1)
-				{
-					cout<<"{"<<i+1<<"-"<<j+1<<"}"<<" ";
-				}
-			}
-		}
-		return false;
-	}
-	for(int j=0;j<n;j++)
-	{
-		if(IsSafe(mat,i,j,n))
-		{
-		  mat[i][j]=1;
-		  bool d=print(mat,i+1,n,c);
-		  if(d==true)
-		  {
-			  return true;//backtrack
-		  }	
-          mat[i][j]=0;		  
-		}
-	}
-	return false;
+
+bool print(int mat[][1000], int i, int n, int &c) {
+    if (i == n) {
+        c++;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    cout << "{" << i + 1 << "-" << j + 1 << "}" << " ";
+                }
+            }
+        }
+        cout << " ";
+        return false;
+    }
+    for (int j = 0; j < n; j++) {
+        if (IsSafe(mat, i, j, n)) {
+            mat[i][j] = 1;
+            bool d = print(mat, i + 1, n, c);
+            if (d == true) {
+                return true;
+            }
+            mat[i][j] = 0;
+        }
+    }
+    return false;
 }
+
 int main() {
-	int n;
-	cin>>n;
-	int mat[1000][1000]={0};
-	int count=0;
-	print(mat,0,n,count);
-	cout<<endl;
-	cout<<count;
-	return 0;
+    int n;
+    cin >> n;
+    int mat[1000][1000] = {0};
+    int count = 0;
+    print(mat, 0, n, count);
+    cout << endl;
+    cout << count;
+    return 0;
 }
